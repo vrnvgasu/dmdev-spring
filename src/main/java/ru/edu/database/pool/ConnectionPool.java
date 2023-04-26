b6e649340e5944c578f576287f46e001e8d986b5
dmitrii sh <dmitrii@sh.com>
2023-04-26 23:48:26 +0300

    8.-Property-Injection

--- src/main/java/ru/edu/database/pool/ConnectionPool.java
+++ src/main/java/ru/edu/database/pool/ConnectionPool.java
@@ -5,15 +5,19 @@ import java.util.Map;
 
 public class ConnectionPool {
 
-	private final String username;
+	private String username;
 	private final Integer poolSize;
-	private final List<Object> args;
-	private final Map<String, Object> properties;
+	private List<Object> args;
+	private Map<String, Object> properties;
 
 	public ConnectionPool(String username, Integer poolSize, List<Object> args, Map<String, Object> properties) {
 		this.username = username;
 		this.poolSize = poolSize;
 		this.args = args;
+		this.properties = properties;
+	}
+
+	public void setProperties(Map<String, Object> properties) {
 		this.properties = properties;
 	}
 
