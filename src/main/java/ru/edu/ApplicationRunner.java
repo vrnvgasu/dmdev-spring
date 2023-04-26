191d57ae7b8936c49332b09aa70654c42516398f
dmitrii sh <dmitrii@sh.com>
2023-04-26 23:08:08 +0300

    6.-Constructor-Injection

--- src/main/java/ru/edu/ApplicationRunner.java
+++ src/main/java/ru/edu/ApplicationRunner.java
@@ -9,6 +9,7 @@ public class ApplicationRunner {
   public static void main(String[] args) {
     ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
     System.out.println(context.getBean("p2", ConnectionPool.class));
+    System.out.println(context.getBean("driver", String.class));
   }
 
 }
