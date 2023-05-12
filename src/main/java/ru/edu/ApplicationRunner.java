8228ed22e6b4aa30cba822857cb90bac6820ac00
dmitrii sh <dmitrii@sh.com>
2023-05-12 23:09:05 +0300

    24.-Java-based-Configuration

--- src/main/java/ru/edu/ApplicationRunner.java
+++ src/main/java/ru/edu/ApplicationRunner.java
@@ -1,7 +1,9 @@
 package ru.edu;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.edu.config.ApplicationConfiguration;
 import ru.edu.database.pool.ConnectionPool;
 import ru.edu.database.repository.CompanyRepository;
 import ru.edu.database.repository.CrudRepository;
@@ -9,9 +11,10 @@ import ru.edu.database.repository.CrudRepository;
 public class ApplicationRunner {
 
   public static void main(String[] args) {
-    try(var context = new ClassPathXmlApplicationContext("application.xml")) {
+//    try(var context = new ClassPathXmlApplicationContext("application.xml")) {
+    try(var context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class)) {
       var connectionPool = context.getBean("pool1", ConnectionPool.class);
-      System.out.println(context.getBean("driver", String.class));
+//      System.out.println(context.getBean("driver", String.class));
 
       var companyRepository = context.getBean("companyRepository", CrudRepository.class);
       System.out.println(companyRepository.findById(1));
---
+++ src/main/java/ru/edu/config/ApplicationConfiguration.java
@@ -0,0 +1,23 @@
+package ru.edu.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+import ru.edu.database.repository.CrudRepository;
+
+@Configuration
+// вместо <context:property-placeholder location="classpath:application.properties"/>
+// можно file или http
+@PropertySource("classpath:application.properties")
+// вместо <context:component-scan
+@ComponentScan(basePackages = "ru.edu", useDefaultFilters = false, includeFilters = {
+  @Filter(type = FilterType.ANNOTATION, value = Component.class),
+  @Filter(type = FilterType.ASSIGNABLE_TYPE, value = CrudRepository.class),
+  @Filter(type = FilterType.REGEX, pattern = "ru\\.edu\\..+Repository")
+})
+public class ApplicationConfiguration {
+
+}
