7766854f6a23e4f4e513938ce79643b62a48c8b7
dmitrii sh <dmitrii@sh.com>
2023-05-15 23:59:03 +0300

    36.-Properties

--- src/main/java/ru/edu/ApplicationRunner.java
+++ src/main/java/ru/edu/ApplicationRunner.java
@@ -2,6 +2,8 @@ package ru.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringProperties;
+import ru.edu.database.pool.ConnectionPool;
 
 @SpringBootApplication
 public class ApplicationRunner {
@@ -9,7 +11,11 @@ public class ApplicationRunner {
   public static void main(String[] args) {
     var context = SpringApplication.run(ApplicationRunner.class, args);
     System.out.println(context.getBeanDefinitionCount());
-    System.out.println(context.getBean("pool1"));
+    var pool = context.getBean("pool1", ConnectionPool.class);
+    System.out.println(pool.getPoolSize());
+    System.out.println(pool.getUsername());
+
+    System.out.println(SpringProperties.getProperty("test.message"));
   }
 
 }
--- src/main/java/ru/edu/database/pool/ConnectionPool.java
+++ src/main/java/ru/edu/database/pool/ConnectionPool.java
@@ -2,12 +2,14 @@ package ru.edu.database.pool;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component("pool1")
 @RequiredArgsConstructor
+@Getter
 public class ConnectionPool {
 
   @Value("${db.username}")
