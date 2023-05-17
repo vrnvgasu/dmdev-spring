d1b57c442ae470c4550d705091b345de32cf981f
dmitrii sh <dmitrii@sh.com>
2023-05-17 22:54:54 +0300

    44.-Integration-Testing.-Part-3

---
+++ src/test/java/ru/edu/integration/annotation/IT.java
@@ -0,0 +1,16 @@
+package ru.edu.integration.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@ActiveProfiles("test")
+@SpringBootTest
+public @interface IT {
+
+}
--- src/test/java/ru/edu/integration/service/CompanyServiceIT.java
+++ src/test/java/ru/edu/integration/service/CompanyServiceIT.java
@@ -2,34 +2,31 @@ package ru.edu.integration.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import ru.edu.ApplicationRunner;
-import ru.edu.database.entity.Company;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.test.context.TestConstructor.AutowireMode;
+import ru.edu.config.DatabaseProperties;
 import ru.edu.dto.CompanyReadDto;
-import ru.edu.listener.entity.EntityEvent;
+import ru.edu.integration.annotation.IT;
 import ru.edu.service.CompanyService;
 
-//@ExtendWith(SpringExtension.class)
-//@ContextConfiguration(classes = ApplicationRunner.class,
-//  // ConfigDataApplicationContextInitializer подключает application.yml
-//  initializers = ConfigDataApplicationContextInitializer.class)
-// вместо @ExtendWith и @ContextConfiguration + работает с application.yml
-@SpringBootTest
+//@ActiveProfiles("test")
+//@SpringBootTest
+@IT // добавили свою аннотацию, чтобы сократить код
+@RequiredArgsConstructor // DI через конструктор
+// можно заменить на spring.test.constructor.autowire.mode=all
+//@TestConstructor(autowireMode = AutowireMode.ALL)
 public class CompanyServiceIT {
 
   private static final Integer COMPANY_ID = 1;
 
-  @Autowired // в тестах нормально делать DI через поля
-  private CompanyService companyService;
+  private final CompanyService companyService;
+  private final DatabaseProperties databaseProperties;
 
   @Test
   void findById() {
@@ -39,6 +36,8 @@ public class CompanyServiceIT {
 
     var expectedResult = new CompanyReadDto(COMPANY_ID);
     actualResult.ifPresent(actual -> assertEquals(expectedResult, actualResult.get()));
+
+    System.out.println(databaseProperties);
   }
 
 }
