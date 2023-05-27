a20fad5dc3ed74713ca280b0e38e3048f1663433
dmitrii sh <dmitrii@sh.com>
2023-05-27 22:51:07 +0300

    54.-RepositoryQuery

--- src/main/java/ru/edu/database/repository/CompanyRepository.java
+++ src/main/java/ru/edu/database/repository/CompanyRepository.java
@@ -1,14 +1,18 @@
 package ru.edu.database.repository;
 
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.Repository;
 import ru.edu.database.entity.Company;
 
 @org.springframework.stereotype.Repository
-public interface CompanyRepository extends Repository<Company, Integer> {
+public interface CompanyRepository extends JpaRepository<Company, Integer> {
 
-  Optional<Company> findById(Integer id);
+  // уже реализованы в JpaRepository
+//  Optional<Company> findById(Integer id);
 
-  void delete(Company id);
+  // уже реализованы в JpaRepository
+//  void delete(Company id);
 
 }
