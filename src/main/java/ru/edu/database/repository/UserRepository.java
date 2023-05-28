4bc42bafc9ccac93a44ad35661d9732282dc2f43
dmitrii sh <dmitrii@sh.com>
2023-05-28 15:23:52 +0300

    59.-Special-parameters

--- src/main/java/ru/edu/database/repository/UserRepository.java
+++ src/main/java/ru/edu/database/repository/UserRepository.java
@@ -1,6 +1,10 @@
 package ru.edu.database.repository;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -34,5 +38,16 @@ public interface UserRepository extends JpaRepository<User, Long> {
     where u.id in (:ids)
     """)
   int updateRole(Role role, Long... ids);
+
+  // первого пользователя с максимальным ID
+  Optional<User> findTopByOrderByIdDesc();
+
+  // 3 пользователя с фильтрацией по birthDate и DESC
+  List<User> findTop3ByBirthDateBeforeOrderByBirthDateDesc(LocalDate birthDate);
+
+  // динамическая сортировка
+  List<User> findTop3ByBirthDateBefore(LocalDate birthDate, Sort sort);
+
+  List<User> findAllBy(Pageable pageable);
 
 }
--- src/test/java/ru/edu/integration/database/repository/UserRepositoryTest.java
+++ src/test/java/ru/edu/integration/database/repository/UserRepositoryTest.java
@@ -2,9 +2,16 @@ package ru.edu.integration.database.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.TypedSort;
 import ru.edu.database.entity.Role;
 import ru.edu.database.entity.User;
 import ru.edu.database.repository.UserRepository;
@@ -15,6 +22,41 @@ import ru.edu.integration.annotation.IT;
 class UserRepositoryTest {
 
   private final UserRepository userRepository;
+
+  @Test
+  void checkPageable() {
+    TypedSort<User> sortBy = Sort.sort(User.class);
+    Sort sort = sortBy.by(User::getFirstname).descending().and(sortBy.by(User::getLastname));
+
+    // вторая страница (отсчет от 0), размер 2, сортировка выше
+    Pageable pageable = PageRequest.of(1, 2, sort);
+
+    var users = userRepository.findAllBy(pageable);
+    Assertions.assertThat(users).hasSize(2);
+  }
+
+  @Test
+  void checkSort() {
+    // хардкорно пишем параметры для сортировки
+//    Sort sort = Sort.by("firstname").descending().and(Sort.by("lastname"));
+
+    // пишем типобезопасно
+    TypedSort<User> sortBy = Sort.sort(User.class);
+    Sort sort = sortBy.by(User::getFirstname).descending().and(sortBy.by(User::getLastname));
+
+    var users = userRepository.findTop3ByBirthDateBefore(LocalDate.now(), sort);
+    System.out.println(users);
+  }
+
+  @Test
+  void checkFirstTop() {
+    Optional<User> topUser = userRepository.findTopByOrderByIdDesc();
+    assertTrue(topUser.isPresent());
+    topUser.ifPresent(user -> assertEquals(5L, user.getId()));
+
+    var users = userRepository.findTop3ByBirthDateBeforeOrderByBirthDateDesc(LocalDate.now());
+    Assertions.assertThat(users).hasSize(3);
+  }
 
   @Test
   void checkUpdate() {
