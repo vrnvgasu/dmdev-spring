4952fcd3f907fb80779aca0af3d7703af30ed618
dmitrii sh <dmitrii@sh.com>
2023-06-08 00:13:13 +0300

    78.-Parameters_-Headers_-Cookies

--- src/main/java/ru/edu/http/controller/GreetingController.java
+++ src/main/java/ru/edu/http/controller/GreetingController.java
@@ -1,10 +1,15 @@
 package ru.edu.http.controller;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import ru.edu.database.repository.CompanyRepository;
 
@@ -15,9 +20,17 @@ public class GreetingController {
   // HandlerMethodArgumentResolver позволяет делать DI многих объектов
   // в методы контроллера. Можно вставлять request, response и любой бин
 //  @RequestMapping(value = "/hello", method = RequestMethod.GET)
-  @GetMapping("/hello")
+  @GetMapping("/hello/{id}")
   public ModelAndView hello(ModelAndView modelAndView,
-    HttpServletRequest request, CompanyRepository companyRepository ) {
+    HttpServletRequest request, @RequestParam(value = "age", required = false) Integer age,
+    @RequestHeader("accept") String accept,
+    @CookieValue("JSESSIONID") String jsessionId,
+    @PathVariable("id") Integer id) {
+    String ageParamValue = request.getParameter("age");
+    String acceptHeader = request.getHeader("accept");
+
+    Cookie[] cookies = request.getCookies();
+
     modelAndView.setViewName("greeting/hello");
     return modelAndView;
   }
