426c1da2bd871e02e8186c451826872b06c1b776
dmitrii sh <dmitrii@sh.com>
2023-06-09 20:01:31 +0300

    85.-Type-Converters

---
+++ src/main/java/ru/edu/config/WebConfiguration.java
@@ -0,0 +1,16 @@
+package ru.edu.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.convert.Jsr310Converters.StringToLocalDateConverter;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+  @Override
+  public void addFormatters(FormatterRegistry registry) {
+    registry.addConverter(StringToLocalDateConverter.INSTANCE);
+  }
+
+}
--- src/main/java/ru/edu/dto/UserCreateEditDto.java
+++ src/main/java/ru/edu/dto/UserCreateEditDto.java
@@ -4,12 +4,14 @@ import lombok.Value;
 
 import java.time.LocalDate;
 import lombok.experimental.FieldNameConstants;
+import org.springframework.format.annotation.DateTimeFormat;
 import ru.edu.database.entity.Role;
 
 @Value
 @FieldNameConstants
 public class UserCreateEditDto {
   String username;
+//  @DateTimeFormat(pattern = "yyyy-MM-dd")
   LocalDate birthDate;
   String firstname;
   String lastname;
