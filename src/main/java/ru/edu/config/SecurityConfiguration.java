a4fe3ef05a37e07cc6b18c6daaa1f9c6d62a9c18
dmitrii sh <dmitrii@sh.com>
2023-06-11 18:15:29 +0300

    103.-HTTP-Basic-Authentication

--- src/main/java/ru/edu/config/SecurityConfiguration.java
+++ src/main/java/ru/edu/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package ru.edu.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -11,6 +12,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf().disable();
     http.authorizeRequests().anyRequest().authenticated();
+
     http.formLogin(login -> login
       // перебрасывать на /login
       .loginPage("/login")
@@ -18,6 +20,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
       .successForwardUrl("/users")
       // даем всем доступ на /login
       .permitAll());
+
+    // HTTP-Basic-Authentication с дефолтными настройками
+//    http.httpBasic(Customizer.withDefaults());
   }
 
 }
