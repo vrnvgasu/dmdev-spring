b568c7d361a0113f88995a7a18493aec15b4038d
dmitrii sh <dmitrii@sh.com>
2023-06-11 19:21:23 +0300

    106.-Authorization-Architecture

--- src/main/java/ru/edu/config/SecurityConfiguration.java
+++ src/main/java/ru/edu/config/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.edu.database.entity.Role;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -14,7 +15,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf().disable();
-    http.authorizeRequests().anyRequest().authenticated();
+//    http.authorizeRequests().anyRequest().authenticated();
+    // проверяем переход по ссылкам на права пользователя
+    http.authorizeHttpRequests(urlConfig -> urlConfig
+      // даем всем доступ
+      .antMatchers("/login", "/users/registration", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
+      // доступ только админам
+      .antMatchers("/users/{\\d+}/delete").hasAuthority(Role.ADMIN.getAuthority())
+      .antMatchers("/admin/**").hasAuthority(Role.ADMIN.getAuthority())
+
+      .anyRequest().authenticated()
+    );
 
     http.logout(logout -> logout
       .logoutUrl("/logout")
@@ -25,9 +36,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
       // перебрасывать на /login
       .loginPage("/login")
       // после успешной аутентификации перебрасывать на /users
-      .successForwardUrl("/users")
-      // даем всем доступ на /login
-      .permitAll());
+      .successForwardUrl("/users"));
 
     // HTTP-Basic-Authentication с дефолтными настройками
 //    http.httpBasic(Customizer.withDefaults());
