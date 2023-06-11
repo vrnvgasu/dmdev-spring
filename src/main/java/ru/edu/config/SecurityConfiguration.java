2bb38177c2894d23d22ea652ee30cef6b453e9ab
dmitrii sh <dmitrii@sh.com>
2023-06-11 23:32:37 +0300

    109.-CSRF-Filter

--- src/main/java/ru/edu/config/SecurityConfiguration.java
+++ src/main/java/ru/edu/config/SecurityConfiguration.java
@@ -16,7 +16,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable();
+//    http.csrf().disable();
 //    http.authorizeRequests().anyRequest().authenticated();
     // проверяем переход по ссылкам на права пользователя
     http.authorizeHttpRequests(urlConfig -> urlConfig
