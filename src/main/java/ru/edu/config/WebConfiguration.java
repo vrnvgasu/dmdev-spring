4d0995a29386e3bce27faaf2de4ba6bfc5fb9171
dmitrii sh <dmitrii@sh.com>
2023-06-12 21:53:38 +0300

    117.-i18n.-Thymeleaf

--- src/main/java/ru/edu/config/WebConfiguration.java
+++ src/main/java/ru/edu/config/WebConfiguration.java
@@ -1,16 +1,46 @@
 package ru.edu.config;
 
+import java.util.Locale;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.convert.Jsr310Converters.StringToLocalDateConverter;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
   @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(localeChangeInterceptor());
+  }
+
+  @Override
   public void addFormatters(FormatterRegistry registry) {
 //    registry.addConverter(StringToLocalDateConverter.INSTANCE);
+  }
+
+  @Bean
+  // где храним локаль пользователя
+  public LocaleResolver localeResolver() {
+    // в куках
+    var localeResolver = new CookieLocaleResolver();
+    localeResolver.setDefaultLocale(Locale.US);
+    return localeResolver;
+  }
+
+  @Bean
+  // этот бин надо явно зарегистрировать в addInterceptors
+  public LocaleChangeInterceptor localeChangeInterceptor() {
+    LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+
+    // будем отслеживать локаль по параметру "lang"
+    interceptor.setParamName("lang");
+    return interceptor;
   }
 
 }
