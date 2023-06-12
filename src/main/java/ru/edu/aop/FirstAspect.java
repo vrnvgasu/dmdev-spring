a2d13bb0b2f916e326aa87f28f29f90c06aa3655
dmitrii sh <dmitrii@sh.com>
2023-06-13 00:02:58 +0300

    119.-AOP.-Pointcut

---
+++ src/main/java/ru/edu/aop/FirstAspect.java
@@ -0,0 +1,70 @@
+package ru.edu.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.data.repository.Repository;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Aspect
+@Component // аспект в спринге должен быть бином
+public class FirstAspect {
+
+  // @within - проверяет, что у класса есть такая аннотация
+  @Pointcut("@within(org.springframework.stereotype.Controller)")
+  public void isControllerLayer() {
+  }
+
+  // within - ищем все классы *Service в пакете ru.edu.service
+  @Pointcut("within(ru.edu.service.*Service)")
+  public void isServiceLayer() {
+  }
+
+  // проверить, что этот объект реализует интерфейс Repository
+  // this - проверяет proxy
+  // target - сам класс
+  @Pointcut("this(org.springframework.data.repository.Repository)")
+//  @Pointcut("target(org.springframework.data.repository.Repository)")
+  public void isRepositoryLayer() {
+  }
+
+  // @annotation - проверяет аннотацию у метода
+//  @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+  // добавили условия Pointcut выше
+  @Pointcut("isControllerLayer() && @annotation(org.springframework.web.bind.annotation.GetMapping)")
+  public void hasGetMapping() {
+  }
+
+  // args - проверяем параметры в методе
+  // * - один любой параметр
+  // .. - любое кол-во любых параметров
+  // проверяем, что в методе первый параметр Model. А дальше неважно сколько всего параметров
+  @Pointcut("isControllerLayer() && args(org.springframework.ui.Model,..)")
+  // проверяем, что в методе первый параметр Model. А всего ровно 3 параметра
+//  @Pointcut("args(org.springframework.ui.Model,*,*)")
+  public void hasModelParam() {
+  }
+
+  // @args - проверяет наличие аннотации над типом класса параметра в методе
+  // внимание! @Validated - это не то. Это не над типом класса параметра
+  // ищем методы, где первый параметр содержит аннотацию UserInfo
+  @Pointcut("isControllerLayer() && @args(ru.edu.validation.UserInfo,..)")
+  public void hasUserInfoParamAnnotation() {
+  }
+
+  // bean - ищет название бина для объекта (не стандарт AspectJ)
+  // все бины с названием *Service
+  @Pointcut("bean(*Service)")
+  public void isServiceLayerBean() {
+  }
+
+  /*
+  ? - может и не быть
+      execution(modifiers-pattern? ret-type-pattern declaring-type-pattern?name-pattern(param-pattern) throws-pattern?)
+   */
+//  @Pointcut("execution(public * findById(*))")
+  @Pointcut("execution(public * ru.edu.service.*Service.findById(*))")
+  public void anyFindByIdServiceMethod() {
+  }
+
+}
