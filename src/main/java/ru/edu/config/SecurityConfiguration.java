89b13c04086dc247f459e7c7d8f20ce6f6a425e4
dmitrii sh <dmitrii@sh.com>
2023-06-12 16:16:25 +0300

    113.-OAuth-2.0.-Authentication-Principle

--- src/main/java/ru/edu/config/SecurityConfiguration.java
+++ src/main/java/ru/edu/config/SecurityConfiguration.java
@@ -1,18 +1,32 @@
 package ru.edu.config;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Set;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import ru.edu.database.entity.Role;
+import ru.edu.service.UserService;
 
 @Configuration
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  private final UserService userService;
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
@@ -43,15 +57,31 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     // подключили авторизацию по OAuth2
     http.oauth2Login(config -> config
       .loginPage("/login")
-      .defaultSuccessUrl("/users"));
+      .defaultSuccessUrl("/users")
+      // меняем объект создания аутентификации
+      .userInfoEndpoint(userInfo -> userInfo.oidcUserService(oidcUserService())));
 
     // HTTP-Basic-Authentication с дефолтными настройками
 //    http.httpBasic(Customizer.withDefaults());
   }
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  private OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
+    return userRequest -> {
+      String email = userRequest.getIdToken().getClaim("email");
+      UserDetails userDetails = userService.loadUserByUsername(email);
+      // TODO if userDetails==null, то userService.create()
+
+      DefaultOidcUser oidcUser = new DefaultOidcUser(userDetails.getAuthorities(), userRequest.getIdToken());
+
+      Set<Method> userDetailsMethods = Set.of(UserDetails.class.getMethods());
+      return (OidcUser) Proxy.newProxyInstance(
+        SecurityConfiguration.class.getClassLoader(),
+        new Class[]{UserDetails.class, OidcUser.class},
+        (proxy, method, args) -> userDetailsMethods.contains(method)
+          ? method.invoke(userDetails, args)
+          : method.invoke(oidcUser, args)
+      );
+    };
   }
 
 }
